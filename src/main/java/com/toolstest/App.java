2760f6e2663ec30bdfcde2bdcab91f9ed38e5401
phanikumar1210 <phanikumardadi.kumar@gmail.com>
2017-06-02 14:07:50 +0530

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -8,5 +8,10 @@ public class App {
 	private String welcome;
     public static void main(String[] args) {
         System.out.println("Hello World!");
+	 int i=1;
+	   if(i==1)
+	   {
+		   System.out.println("welcome");
+	   }
     }
 }
