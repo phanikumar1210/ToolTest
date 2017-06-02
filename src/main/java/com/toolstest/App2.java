1f7987b0be85dda17be0b45343212ca121e78bf0
phanikumar1210 <phanikumardadi.kumar@gmail.com>
2017-06-02 14:55:09 +0530

    Create App2.java

---
+++ src/main/java/com/toolstest/App2.java
@@ -0,0 +1,17 @@
+package com.toolstest;
+
+/**
+ * Hello world!
+ */
+ //welcome to sample djdkf
+public class App2 {
+	private String welcome;
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+	 int i=1;
+	   if(i==1)
+	   {
+		   System.out.println("welcome");
+	   }
+    }
+}
